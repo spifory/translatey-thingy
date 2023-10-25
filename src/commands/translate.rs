2e6737535d62e3e3174b8ec787afd0580c349b91
spifory <luvuyosilubane50@gmail.com>
2023-10-25 21:56:24 +0200

    feat(commands/translate): send and log occurred errors

--- src/commands/translate.rs
+++ src/commands/translate.rs
@@ -1,6 +1,6 @@
 use google_translator::{translate_one_line, InputLang, OutputLang};
-use poise::serenity_prelude::Mentionable;
 use poise::{command, serenity_prelude::User};
+use serenity::prelude::Mentionable;
 
 use crate::command_types;
 
@@ -15,28 +15,27 @@ pub async fn translate_nick(
     ctx: command_types::Context<'_>,
     #[description = "The user who's nickname to translate."] user: User,
 ) -> Result<(), command_types::Error> {
-    let user_nick = user.nick_in(ctx, ctx.guild_id().unwrap()).await.unwrap();
+    let user_nick = user.nick_in(ctx, ctx.guild_id().unwrap()).await;
 
-    let translated_nickname =
-        translate_one_line(user_nick, InputLang::Norwegian, OutputLang::English)
-            .await
-            .unwrap_or(format!(
-                "{}'s nickname could not be translated",
-                user.mention()
-            ));
-
-    let response = format!(
-        "{}'s nickname from **Norwegian** to **English** is (_probably_) \"{}\"",
-        user.mention(),
-        translated_nickname
-    );
-
-    ctx.send(|reply| {
-        reply
-            .content(response)
-            .allowed_mentions(|am| am.empty_users())
-    })
-    .await?;
-
-    Ok(())
+    if user_nick.is_none() {
+        let content = format!("{} has no nickname to be translated", user.mention());
+        let _ = ctx
+            .send(|b| b.content(content).allowed_mentions(|am| am.empty_users()))
+            .await;
+        Ok(())
+    } else {
+        match translate_one_line(user_nick.unwrap(), InputLang::Norwegian, OutputLang::English).await {
+            Err(err) => {
+                let content = format!("{}'s nickname could not be translated: {}", user.mention(), err);
+                log::error!("{}", content);
+                let _ = ctx.send(|b| b.content(content).allowed_mentions(|am| am.empty_users())).await;
+                Ok(())
+            },
+            Ok(translated_nick) => {
+                let content = format!("{}'s nickname from **Norwegian** to **English** is _probably_ \"{}\"", user.mention(), translated_nick);
+                let _ = ctx.send(|b| b.content(content).allowed_mentions(|am| am.empty_users())).await;
+                Ok(())
+            }
+        }
+    }
 }
